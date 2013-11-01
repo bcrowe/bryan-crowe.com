91965046499ea455a229510eac33d25bff645e93
Bryan Crowe <bryan@zapsolutions.com>
2013-10-31 20:29:41 -0400

    Add user login

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -9,7 +9,21 @@ App::uses('AppController', 'Controller');
 class UsersController extends AppController {
 
 	public function beforeFilter() {
-		$this->Auth->allow(['index', 'add']);
+		$this->Auth->allow(['index', 'add', 'login']);
+	}
+
+	public function login() {
+		if ($this->request->is('post')) {
+			if ($this->Auth->login()) {
+				return $this->redirect($this->Auth->redirect());
+			}
+			$this->Session->setFlash(__('Invalid username or password, try again.'));
+		}
+	}
+
+	public function logout() {
+		$this->Session->destroy();
+		return $this->redirect($this->Auth->logout());
 	}
 
 }
