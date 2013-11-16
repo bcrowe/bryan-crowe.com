ef93caa11d82b6c835020feca08643dc44f48c0c
Bryan Crowe <bryan@bryan-crowe.com>
2013-11-16 18:56:40 -0500

    Begin GeSHi implementation

---
+++ app/Config/geshi.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Sample / test config file.
+ */
+
+$geshi->set_header_type(GESHI_HEADER_NONE);
+$geshi->enable_line_numbers(GESHI_FANCY_LINE_NUMBERS, 1);
+$geshi->enable_classes();
+$geshi->set_tab_width(4);
