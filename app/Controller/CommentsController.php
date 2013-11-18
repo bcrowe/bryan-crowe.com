d755817eaf4d42e8364311e1e4d573d039a7781c
Bryan Crowe <bryan@bryan-crowe.com>
2013-11-17 23:02:12 -0500

    Add comments cake stuff

---
+++ app/Controller/CommentsController.php
@@ -0,0 +1,11 @@
+<?php
+App::uses('AppController', 'Controller');
+/**
+ * Comments Controller
+ *
+ * @property Comment $Comment
+ * @property PaginatorComponent $Paginator
+ */
+class CommentsController extends AppController {
+
+}
---
+++ app/Model/Comment.php
@@ -0,0 +1,74 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * Comment Model
+ *
+ * @property Post $Post
+ */
+class Comment extends AppModel {
+
+/**
+ * Validation rules
+ *
+ * @var array
+ */
+	public $validate = array(
+		'name' => array(
+			'notEmpty' => array(
+				'rule' => array('notEmpty'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'email' => array(
+			'email' => array(
+				'rule' => array('email'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'comment' => array(
+			'notEmpty' => array(
+				'rule' => array('notEmpty'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'post_id' => array(
+			'uuid' => array(
+				'rule' => array('uuid'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+	);
+
+	//The Associations below have been created with all possible keys, those that are not needed can be removed
+
+/**
+ * belongsTo associations
+ *
+ * @var array
+ */
+	public $belongsTo = array(
+		'Post' => array(
+			'className' => 'Post',
+			'foreignKey' => 'post_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		)
+	);
+}
