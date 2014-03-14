4fc294ad2e391b034854bc2fe0656a2405e832ad
Bryan Crowe <bryan@bryan-crowe.com>
2014-03-14 14:26:45 -0400

    Add docblocks to PostsController actions

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -18,6 +18,9 @@ class PostsController extends AppController {
 		$this->Auth->allow(['posts', 'view']);
 	}
 
+	/**
+	 * Retrieve all posts for the front page
+	 */
 	public function posts() {
 		$posts = $this->Post->find('all', [
 			'order' => 'Post.created DESC'
@@ -26,11 +29,26 @@ class PostsController extends AppController {
 		$this->set(compact('posts', 'title_for_layout'));
 	}
 
+	/**
+	 * Retrieve a single post for full view
+	 *
+	 * @param string|null $slug The post's URL slug
+	 * @throws NotFoundException If a slug is not provided or invalid
+	 */
 	public function view($slug = null) {
+		if ($slug === null) {
+			throw new NotFoundException(__('Invalid post.'));
+		}
+
 		$post = $this->Post->find('first', [
 			'conditions' => ['Post.slug' => $slug],
 			'contain' => ['Comment']
 		]);
+
+		if (!$post) {
+			throw new NotFoundException(__('Invalid post.'));
+		}
+
 		$title_for_layout = $post['Post']['title'] . ' | Bryan Crowe';
 		$this->set(compact('post', 'title_for_layout'));
 	}
